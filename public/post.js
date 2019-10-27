1c2e890b6307f3b1b35b5a6a1b8249eba6059c33
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-10-27 11:05:28 -0400

    finished up the post.js in the public folder

---
+++ public/post.js
@@ -0,0 +1,95 @@
+// fields
+const $distance = $('#distance');
+const $minutes = $('#minutes');
+const $seconds = $('#seconds');
+const $weight = $('#weight');
+const $reps = $('#reps');
+
+// space to tell user info
+const $alert = $('#alert');
+
+// submit buttons
+const $submitRun = $('#submit-run');
+const $submitBike = $('#submit-bike');
+const $submitWeight = $('#submit-weight');
+
+// function that posts for every type
+function ajaxPost(route, object) {
+  $.post(`/api/${route}`, object)
+    .then((data) => {
+      window.location.reload(true);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// function that creates run object and posts
+function postRun() {
+  const distance = $distance.val().trim();
+  const minutes = $minutes.val().trim();
+  const seconds = $seconds.val().trim();
+
+  if (isNaN(distance) || distance < 0) {
+    $alert.text('The distance you entered is not valid');
+  } else if (isNaN(minutes) || minutes < 0) {
+    $alert.text('The minutes you entered are not valid');
+  } else if (isNaN(seconds) || seconds < 0) {
+    $alert.text('The seconds you entered are not valid');
+  } else {
+    const runObject = {
+      distance,
+      minutes,
+      seconds,
+    };
+
+    ajaxPost('run', runObject);
+  }
+}
+
+// creates biuke object and posts
+function postBike() {
+  const distance = $distance.val().trim();
+  const minutes = $minutes.val().trim();
+  const seconds = $seconds.val().trim();
+
+  if (isNaN(distance) || distance < 0) {
+    $alert.text('The distance you entered is not valid');
+  } else if (isNaN(minutes) || minutes < 0) {
+    $alert.text('The minutes you entered are not valid');
+  } else if (isNaN(seconds) || seconds < 0) {
+    $alert.text('The seconds you entered are not valid');
+  } else {
+    const bikeObject = {
+      distance,
+      minutes,
+      seconds,
+    };
+
+    ajaxPost('bike', bikeObject);
+  }
+}
+
+// create weight object and psot
+function postWeight() {
+  const weight = $weight.val().trim();
+  const reps = $reps.val().trim();
+
+  if (isNaN(weight) || weight < 1) {
+    $alert.text('The weight you entered is not valid');
+  } else if (isNaN(reps) || reps < 1) {
+    $alert.text('The reps you entered are not valid');
+  } else {
+    const weightObject = {
+      weight,
+      reps,
+    };
+
+    ajaxPost('weight', weightObject);
+  }
+}
+
+// listeners
+$submitRun.on('click', postRun);
+$submitBike.on('click', postBike);
+$submitWeight.on('click', postWeight);
