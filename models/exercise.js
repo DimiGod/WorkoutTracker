7b77bfa1e74d989acd6215e3fb00f76eb80d22df
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-10-29 21:27:28 -0400

    make it better

--- models/exercise.js
+++ models/exercise.js
@@ -16,14 +16,14 @@ const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 module.exports = Exercise;
 
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const NoteSchema = new Schema({
-  title: String,
-  body: String
-});
+// const NoteSchema = new Schema({
+//   title: String,
+//   body: String
+// });
 
-const Note = mongoose.model("Note", NoteSchema);
+// const Note = mongoose.model("Note", NoteSchema);
 
-module.exports = Note;
+// module.exports = Note;
